2b9157d4ab59bb21b1a550389562cb6da2f96766
Andrew Marmion <andrew.marmion@gmail.com>
2020-12-22 17:17:54 +0000

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Andrew Marmion on 22/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
